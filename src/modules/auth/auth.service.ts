4ea6c7c44a43e64e3649bf2d6e95374840de2f9d
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-11 15:45:43 -0300

    feat: return userId

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -22,8 +22,10 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
+
     return {
       access_token: this.jwtService.sign(payload),
+      userId: user.id,
     };
   }
 }
